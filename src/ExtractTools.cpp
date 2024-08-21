d086f8258636f6423b4ef04fdec6c2639aa5ea0f
zhoulei-Ckjs <56968749@qq.com>
2024-08-21 15:22:57 +0800

    修复sub-module为空时匹配到下一行的*问题

--- src/ExtractTools.cpp
+++ src/ExtractTools.cpp
@@ -5,7 +5,7 @@
 std::map<std::string, std::string> ExtractTools::ExtractParamsContent(const std::string& commentText)
 {
     /// 正则表达式用于匹配 @param 后跟参数名和描述
-    std::regex param_regex(R"(@param\s+(\S+)\s+(.+))");
+    std::regex param_regex(R"(@param[ \t]+(\S+)\s+(.+))");
     std::sregex_iterator it(commentText.begin(), commentText.end(), param_regex);
     std::sregex_iterator end;
 
@@ -22,7 +22,7 @@ std::map<std::string, std::string> ExtractTools::ExtractParamsContent(const std:
 
 std::string ExtractTools::ExtractReturnContent(const std::string& commentText)
 {
-    std::regex return_regex(R"(@return\s+(\S+))");
+    std::regex return_regex(R"(@return[ \t]+(\S+))");
     std::smatch match;
     if(std::regex_search(commentText, match, return_regex))
     {
@@ -38,7 +38,7 @@ std::string ExtractTools::ExtractReturnContent(const std::string& commentText)
  */
 std::string ExtractTools::ExtractBriefContent(const std::string& commentText)
 {
-    std::regex brief_regex(R"(@brief\s+(\S+))");
+    std::regex brief_regex(R"(@brief[ \t]+(\S+))");
     std::smatch match;
     if(std::regex_search(commentText, match, brief_regex))
     {
@@ -54,7 +54,7 @@ std::string ExtractTools::ExtractBriefContent(const std::string& commentText)
  */
 std::string ExtractTools::ExtractNameContent(const std::string& commentText)
 {
-    std::regex class_regex(R"(@name\s+(\S+))");
+    std::regex class_regex(R"(@name[ \t]+(\S+))");
     std::smatch match;
     if(std::regex_search(commentText, match, class_regex))
     {
@@ -70,7 +70,7 @@ std::string ExtractTools::ExtractNameContent(const std::string& commentText)
  */
 std::string ExtractTools::ExtractSubModuleContent(const std::string& commentText)
 {
-    std::regex subModuleRegex(R"(@sub_module\s+(\S+))");
+    std::regex subModuleRegex(R"(@sub_module[ \t]+(\S+))");
     std::smatch match;
     if(std::regex_search(commentText, match, subModuleRegex))
     {
@@ -81,7 +81,7 @@ std::string ExtractTools::ExtractSubModuleContent(const std::string& commentText
 
 std::string ExtractTools::ExtractModuleContent(const std::string &commentText)
 {
-    std::regex moduleRegex(R"(@module\s+(\S+))"); ///< 这是一个原始字符串字面量，表示正则表达式的内容。原始字符串字面量以 R"( 开始，并以 )" 结束，中间的内容不会对转义字符进行处理。
+    std::regex moduleRegex(R"(@module[ \t]+(\S+))"); ///< 这是一个原始字符串字面量，表示正则表达式的内容。原始字符串字面量以 R"( 开始，并以 )" 结束，中间的内容不会对转义字符进行处理。
     std::smatch match;
     if (std::regex_search(commentText, match, moduleRegex))
     {
@@ -92,7 +92,7 @@ std::string ExtractTools::ExtractModuleContent(const std::string &commentText)
 
 std::string ExtractTools::ExtractSystemContent(const std::string &commentText)
 {
-    std::regex system_regex(R"(@system\s+(\S+))");
+    std::regex system_regex(R"(@system[ \t]+(\S+))");
     std::smatch match;
     if(std::regex_search(commentText, match, system_regex))
     {
