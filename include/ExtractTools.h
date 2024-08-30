52e4e0cf9bcedabda14bf83257aabd755b5e4529
zhoulei-Ckjs <56968749@qq.com>
2024-08-30 09:12:14 +0800

    增加类

--- include/ExtractTools.h
+++ include/ExtractTools.h
@@ -7,6 +7,18 @@ class ExtractTools
 {
 public :
     /**
+     * @brief 抽取 brief 的内容
+     * @param commentText 注释字符串
+     * @return 注释中 @name 指定的内容
+     */
+    static std::string ExtractBriefContent(const std::string& commentText);
+    /**
+     * @brief 抽取类内容
+     * @param commentText 注释
+     * @return 类注释中 @name 指定的内容
+     */
+    static std::string ExtractClassContent(const std::string& commentText);
+    /**
      * @brief 抽取子模块
      * @param commentText
      * @return @sub_module的内容
--- src/CppParser.cpp
+++ src/CppParser.cpp
@@ -2,6 +2,15 @@
 
 namespace CPPPARSER
 {
+    class_::class_(std::string packageName, std::string class_name,
+                   std::string system, std::string module, std::string sub_module, std::string name,
+                   std::string description)
+            : _packageName(packageName), _class_name(class_name), _system(system),
+              _module(module), _sub_module(sub_module), _name(name), _description(description)
+    {
+        ignore = false;
+    }
+
     module::module(std::string name) : _name(name)
     {
 
