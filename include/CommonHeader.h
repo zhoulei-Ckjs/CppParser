a2745afa44839003b171fc80a64af5787a733d69
zhoulei-Ckjs <56968749@qq.com>
2024-11-20 04:42:55 +0800

    增加标准库类型

--- include/CommonHeader.h
+++ include/CommonHeader.h
@@ -5,7 +5,69 @@
 
 std::map<std::string, std::string> g_header_substitute =
         {
-                {R"(\s*#include\s*<map>)", "namespace std{template<typename... Args> class map{};}"}
+                {R"(\s*#include\s*<list>)", R"(
+#ifndef __ISCAS_STD_LIST__
+#define __ISCAS_STD_LIST__
+namespace std
+{
+    template<typename... Args>
+    class list{};
+}
+#endif
+)"},
+                {R"(\s*#include\s*<map>)", R"(
+#ifndef __ISCAS_STD_MAP__
+#define __ISCAS_STD_MAP__
+namespace std
+{
+    template<typename... Args>
+    class map{};
+}
+#endif
+)"},
+                {R"(\s*#include\s*<pthread.h>)", R"(
+#ifndef __ISCAS_PTHREAD_MUTEX_T__
+#define __ISCAS_PTHREAD_MUTEX_T__
+class pthread_mutex_t{};
+class pthread_mutexattr_t{};
+class pthread_rwlock_t{};
+#endif
+)"},
+                {R"(\s*#include\s*<string>)", R"(
+#ifndef __ISCAS_STRING__
+#define __ISCAS_STRING__
+namespace std
+{
+    class string{};
+}
+#endif
+)"},
+                {R"(\s*#include\s*<sys/time.h>)", R"(
+#ifndef __ISCAS_SYS_TIME_H__
+#define __ISCAS_SYS_TIME_H__
+typedef struct timeval
+{}timeval;
+#endif
+)"},
+                {R"(\s*#include\s*<thread>)", R"(
+#ifndef __ISCAS_STD_THREAD__
+#define __ISCAS_STD_THREAD__
+namespace std
+{
+    class thread{};
+}
+#endif
+)"},
+                {R"(\s*#include\s*<vector>)", R"(
+#ifndef __ISCAS_STD_VECTOR__
+#define __ISCAS_STD_VECTOR__
+namespace std
+{
+    template<typename... Args>
+    class vector{};
+}
+#endif
+)"}
         };
 
 #endif //FUNCTIONPARSER_COMMONHEADER_H
